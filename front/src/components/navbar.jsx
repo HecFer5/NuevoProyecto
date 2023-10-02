8f0588ca80f47a7c5e23bcb062236919b24f59b9
HecFer5 <hecalfer@hotmail.com>
2023-10-02 18:12:27 -0300

    segundo del dia 2/10

--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -1,35 +1,40 @@
 import React from 'react'
-import { Container, LogoContainer, Menu, MenuItem, MenuItemLink, Wrapper } from './navbarElements'
+import { Container, LogoContainer, Menu, MenuItem, MenuItemLink, Wrapper, MobileIcon } from './navbarElements'
 import { PiFlowerTulipBold } from "react-icons/pi";
-import {IconContext} from 'react-icons'
+import { IconContext } from 'react-icons'
+import { FaAlignJustify } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <Container>
             <Wrapper>
-                <IconContext.Provider value= {{style: {fontSize: "2em" }}}>
-                <LogoContainer>
-                    <PiFlowerTulipBold />
-                    <p>Vector -</p>
-                    <p>f(x)</p>
-                </LogoContainer>
-                <Menu>
-                    <MenuItem>
-                        <MenuItemLink>
-                            HOME
-                        </MenuItemLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuItemLink>
-                           VER TODO
-                        </MenuItemLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuItemLink>
-                            NUEVO INGRESO
-                        </MenuItemLink>
-                    </MenuItem>
-                </Menu>
+                <IconContext.Provider value={{ style: { fontSize: "2em" } }}>
+                    <LogoContainer>
+                        <PiFlowerTulipBold />
+                        <p>Vector -</p>
+                        <p>f(x)</p>
+                    </LogoContainer>
+                    <MobileIcon>
+                        <FaAlignJustify/>
+                    </MobileIcon>
+                    <Menu>
+                        <MenuItem>
+                            <MenuItemLink>
+
+                                HOME
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink to={"/muestra"}>
+                                VER TODO
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink to={'/create'}>
+                                NUEVO INGRESO
+                            </MenuItemLink>
+                        </MenuItem>
+                    </Menu>
                 </IconContext.Provider>
             </Wrapper>
         </Container>
--- front/src/components/navbarElements.jsx
+++ front/src/components/navbarElements.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import {Link} from 'react-router-dom'
 
 export const Container = styled.div`
 width: 100%;
@@ -45,13 +46,28 @@ height: 100%;
 display: flex;
 justify-content: space-between;
 list-style: none;
+
+@media screen and (max-width:960px) {
+    position: absolute;
+    top: 70px;
+    left: 0;
+    width: 100%;
+    height: 90vh;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    transition: 0.5s all ease;
+    background-color: #23394d;
+
+    
+}
     
 `
 export const MenuItem = styled.li`
 height: 100%;
     
 `
-export const MenuItemLink = styled.a`
+export const MenuItemLink = styled(Link)`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -68,5 +84,21 @@ export const MenuItemLink = styled.a`
         color: #fff;
         background-color: #e8792a;
         transition: 0.5s all ease;
+    }
+`
+
+export const MobileIcon = styled.div`
+    display: none;
+
+    @media screen and (max-width: 960px){
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+
+        svg{
+            fill: #e07924;
+            margin-right: 0.5;
+        }
+
     }
 `
